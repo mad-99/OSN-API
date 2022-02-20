73be7d141b23102a26ae269c1d874c223394a339
MADHUR MAHESHWARI <72245519+mad-99@users.noreply.github.com>
2022-02-20 21:14:02 +0530

    Update Madfacebook.py

--- Madfacebook.py
+++ Madfacebook.py
@@ -2,7 +2,7 @@ import json
 import facebook
 
 def main():
-	token = "EAAGdZB8LwJzwBADBX71ZCBOmvpWCjYifZAQzZC3GwSq5R5UvTy9sbSwKg3rHVbGyh5FGWCkX4jIRxRn8vZBZBrVNHo77XGdstNsQhdWw3r5YOBjyAatEz7cX0LFFgpF2FGPKVFLzmSsJhZA3W3RZBjlLUw4PvHMAmhFJnkCn8zShQQZDZD"
+	token = "____________________________________secret_key________________________________________________"
 	graph = facebook.GraphAPI(token)
 	#fields = ['first_name', 'location{location}','email','link']
 	profile = graph.get_object('me',fields='first_name,last_name,birthday,hometown,age_range,gender,location{location},email,likes,posts,photos,friends')	
